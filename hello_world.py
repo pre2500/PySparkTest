d3fd4df28eb6242282652e0b64e853e8776dc4e7
pre2500 <pre2500@users.noreply.github.com>
2020-03-10 08:45:15 -0700

    commit

--- hello_world.py
+++ hello_world.py
@@ -1,20 +1,22 @@
 import sys
-from random import random
-from operator import add
+from random
+import random
+from operator
+import add
 
-from pyspark.sql import SparkSession
+from pyspark.sql
+import SparkSession
 
 spark = SparkSession\
-.builder\
-.appName("HelloWorld")\
-.getOrCreate()
- 
+  .builder\
+  .appName("HelloWorld")\
+  .getOrCreate()
 
 data = spark.parallelize(list("Hello World"))
-counts = data.map(lambda x: 
-	(x, 1)).reduceByKey(add).sortBy(lambda x: x[1],
-	 ascending=False).collect()
+counts = data.map(lambda x:
+  (x, 1)).reduceByKey(add).sortBy(lambda x: x[1],
+  ascending = False).collect()
 
 for (word, count) in counts:
-    print("{}: {}".format(word, count))
+  print("{}: {}".format(word, count))
 
