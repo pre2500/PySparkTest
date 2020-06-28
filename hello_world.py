ede48f3a66aa1d55e4d2614e34313351621b6d63
pre2500 <pre2500@users.noreply.github.com>
2020-06-28 01:55:33 -0700

    Update hello_world.py
    commit

--- hello_world.py
+++ hello_world.py
@@ -1,4 +1,6 @@
 from pyspark.sql import SparkSession
+import os
+
 
 def init_spark():
   spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
@@ -9,6 +11,8 @@ def main():
   spark,sc = init_spark()
   nums = sc.parallelize([1,2,3,4])
   print(nums.map(lambda x: x*x).collect())
+  for param in os.environ.keys():
+    print "%20s %s" % (param,os.environ[param])
 
 
 if __name__ == '__main__':
