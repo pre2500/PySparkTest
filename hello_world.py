5c34b290b5e38e0722c166973f84a7232256fa0f
pre2500 <pre2500@users.noreply.github.com>
2020-06-30 00:07:35 -0700

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 from pyspark.sql import SparkSession
 import os
 import time
@@ -6,30 +8,32 @@ import botocore
 
 
 def init_spark():
-    spark = SparkSession.builder.appName("HelloWorld").getOrCreate()
+    spark = SparkSession.builder.appName('HelloWorld').getOrCreate()
     sc = spark.sparkContext
-    return spark, sc
+    return (spark, sc)
 
 
 def main():
-    spark, sc = init_spark()
+    (spark, sc) = init_spark()
     nums = sc.parallelize([1, 2, 3, 4])
     time.sleep(300)
-    print(nums.map(lambda x: x * x).collect())
-    print(os.getenv('SPARK_SUBMIT_OPTS'))
+    print nums.map(lambda x: x * x).collect()
+    print os.getenv('SPARK_SUBMIT_OPTS')
     download_file_from_s3()
 
 
 def download_file_from_s3():
     s3 = boto3.resource('s3')
     try:
-        s3.Bucket('tardis-im-clients').download_file(KEY, 'keycloak_client_list.csv')
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
+        s3.Bucket('tardis-im-clients').download_file(KEY,
+                'keycloak_client_list.csv')
+    except botocore.exceptions.ClientError, e:
+        if e.response['Error']['Code'] == '404':
+            print 'The object does not exist.'
         else:
             raise
 
 
 if __name__ == '__main__':
     main()
+
